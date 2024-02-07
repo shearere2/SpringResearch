7fdede3c3a675d3bd025162028663c5ef00b4e72
shearere2 <shearere2@duq.edu>
2024-02-07 14:31:06 -0500

    Added a few basic ways to map bus routes

--- SpringResearch/map.py
+++ SpringResearch/map.py
@@ -16,9 +16,12 @@ import pickle
 if __name__ == "__main__":
     #print(df.crs) # Gives Coordinate Reference System
     df = gpd.read_file('/Users/shearer/Desktop/SpringResearch/data/City_of_Pittsburgh_Neighborhoods/City_of_Pittsburgh_Neighborhoods.shp')
-    stops = gpd.read_file('/Users/shearer/Desktop/SpringResearch/data/Data/paac_stops_1909/PAAC_Stops_1909.shp')
+    stops = gpd.read_file('/Users/shearer/Desktop/SpringResearch/data/Data/PAAC_Stops_1909-shp.zip')
 
-    burgh = geoplot.polyplot(df,projection=gcrs.AlbersEqualArea())
-    burgh_stops = geoplot.pointplot(stops, ax=burgh).figure
+    burgh = geoplot.polyplot(df,projection=gcrs.AlbersEqualArea(),figsize = (96,72))
+    burgh_stops = geoplot.pointplot(stops, ax=burgh, ).figure
     burgh_stops.savefig('data/file1.png')
 
+
+    # Also, simple note, this automatically shows us a more complicated web map
+    # geoplot.webmap(df,projection=gcrs.WebMercator(),figsize=(100,100)).figure.savefig('data/file2.png')
