a9e5bfbd041c5f6fe8f5adc801a5a71e2257623b
shearere2 <shearere2@duq.edu>
2024-02-12 16:33:17 -0500

    - Adjusted csv merger function to actually work - Still working on it, work in progress - Attemped to map variables from merged data frame - No success, geojson is hard to figure out.

--- SpringResearch/csv_merger.py
+++ SpringResearch/csv_merger.py
@@ -1,43 +1,21 @@
-import os
-def combined_csv(path: str):
-    """Function created to take in a path to a folder with a lot of .csv files,
-    and it merges the files on a common column. Credit to
-    https://raredogmarketing.com/resources/combining-multiple-csv-files-into-one-file-using-python-step-by-step-guide
-    for a majority of this function.
+import pandas as pd
+def combined_csv(path_list: list):
+    pass
 
-    Args:
-        path (str): The folder path
-    """
-    folder_path = path
+if __name__ == "__main__":
+    pitt = pd.read_csv('/Users/shearer/Desktop/SpringResearch/data/City_of_Pittsburgh_Neighborhoods/City_of_Pittsburgh_Neighborhoods.csv')   
+    pitt = pitt.rename(columns = {'HOOD':'Neighborhood'}).set_index('Neighborhood')
 
-    all_files = os.listdir(folder_path)
-
-    # Filter out non-CSV files
-    csv_files = [f for f in all_files if f.endswith('.csv')]
-
-    # Create a list to hold the dataframes
-    df_list = []
-    print(csv_files)
-
-    for csv in csv_files:
-        file_path = os.path.join(folder_path, csv)
-        try:
-            # Try reading the file using default UTF-8 encoding
-            df = pd.read_csv(file_path)
-            df_list.append(df)
-        except UnicodeDecodeError:
-            try:
-                # If UTF-8 fails, try reading the file using UTF-16 encoding with tab separator
-                df = pd.read_csv(file_path, sep='\t', encoding='utf-16')
-                df_list.append(df)
-            except Exception as e:
-                print(f"Could not read file {csv} because of error: {e}")
-        except Exception as e:
-            print(f"Could not read file {csv} because of error: {e}")
-
-    # Concatenate all data into one DataFrame    
-    from functools import reduce
-    df_merged = reduce(lambda  left,right: pd.merge(left,right,on=['Neighborhood'],
-                                            how='outer'),df_list)
-    # Save the final result to a new CSV file
-    df_merged.to_csv(os.path.join(folder_path, 'combined_file.csv'), index=False)
+    path_list = ['/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/built-enviornment-conditions.csv',
+                '/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/education-income.csv',
+                '/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/employment.csv',
+                '/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/housing (1).csv',
+                '/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/land-use-zoning.csv',
+                '/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/natural-environment-conditions.csv',
+                '/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/population-density.csv',
+                '/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/public-safety.csv',
+                '/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/transportation.csv']         
+    for path in path_list:
+        df = pd.read_csv(path)
+        pitt = pitt.merge(df,on = 'Neighborhood', how = 'outer')
+        pitt.to_csv('/Users/shearer/Desktop/SpringResearch/data/test1.csv', index=False)
--- SpringResearch/map.py
+++ SpringResearch/map.py
@@ -22,18 +22,20 @@ if __name__ == "__main__":
 
 
 #    #Also, simple note, this automatically shows us a more complicated web map
-#     geoplot.webmap(df,projection=gcrs.WebMercator(),figsize=(100,100)).figure.savefig('data/file2.png')
+#    geoplot.webmap(df,projection=gcrs.WebMercator(),figsize=(100,100)).figure.savefig('data/file2.png')
 
 
-    hoods = pd.read_csv('/Users/shearer/Desktop/SpringResearch/data/City_of_Pittsburgh_Neighborhoods/City_of_Pittsburgh_Neighborhoods.csv')
-    geojson = gpd.read_file('/Users/shearer/Desktop/SpringResearch/data/City_of_Pittsburgh_Neighborhoods/City_of_Pittsburgh_Neighborhoods.geojson')
+    hoods = pd.read_csv('/Users/shearer/Desktop/SpringResearch/data/test1.csv')
+    geojson = gpd.read_file('/Users/shearer/Desktop/SpringResearch/data/test1.geojson')
 
-    fig = px.choropleth_mapbox(hoods, geojson=geojson, color="AREA",
-                           locations="HOOD", featureidkey="properties.HOOD",
+    fig = px.choropleth_mapbox(hoods, geojson=geojson, color="OBJECTID",
+                           locations="Neighborhood", featureidkey="properties.Neighborhood",
                            center={"lat": 40.440624, "lon": -79.995888},
                            mapbox_style="carto-positron", zoom=9)
     fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
-    stops = gpd.read_file('/Users/shearer/Desktop/SpringResearch/data/paac_routes_1909/PAAC_Routes_1909.csv')
+    stops = pd.read_csv('/Users/shearer/Desktop/SpringResearch/data/paac_stops_1909/PAAC_Stops_1909.csv')
     # Try to add stops
-    fig.add_layer(px.geo_scatter(stops))
+
+    # fig.add_scattergeo(px.scatter_geo(stops))
+    fig.show()
