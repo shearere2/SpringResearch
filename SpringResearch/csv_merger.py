5e787494db5d38556e36fcab3ba3f16113d528f6
shearere2 <shearere2@duq.edu>
2024-02-13 13:48:31 -0500

    Finalized perfecting merger function

--- SpringResearch/csv_merger.py
+++ SpringResearch/csv_merger.py
@@ -1,5 +1,12 @@
 import pandas as pd
 def csv_merge(large: pd.DataFrame, paths: list):
+    """Sequentially merges a list of csv files using pd.merge
+    and saves to working directory
+
+    Args:
+        large (pd.DataFrame): the data frame that will be passed in as the original
+        paths (list): list of file paths for the csvs to merge.
+    """
     for path in paths:
         df = pd.read_csv(path)
         large = large.merge(df,on = 'Neighborhood', how = 'outer')
@@ -7,7 +14,7 @@ def csv_merge(large: pd.DataFrame, paths: list):
 
 if __name__ == "__main__":
     pitt = pd.read_csv('data/City_of_Pittsburgh_Neighborhoods/City_of_Pittsburgh_Neighborhoods.csv')   
-    pitt = pitt.rename(columns = {'HOOD':'Neighborhood'}).set_index('Neighborhood')
+    pitt = pitt.set_index('Neighborhood')
     path_list = ['data/Data/to_combine/built-environment-conditions.csv',
                 'data/Data/to_combine/education-income.csv',
                 'data/Data/to_combine/employment.csv',
