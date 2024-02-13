98281d16213017b226f5db3c973e6b133d807557
shearere2 <shearere2@duq.edu>
2024-02-13 13:15:48 -0500

    Finally got the csv merger function to be built correctly

--- SpringResearch/csv_merger.py
+++ SpringResearch/csv_merger.py
@@ -1,18 +1,21 @@
 import pandas as pd
-if __name__ == "__main__":
-    pitt = pd.read_csv('/Users/shearer/Desktop/SpringResearch/data/City_of_Pittsburgh_Neighborhoods/City_of_Pittsburgh_Neighborhoods.csv')   
-    pitt = pitt.rename(columns = {'HOOD':'Neighborhood'}).set_index('Neighborhood')
-
-    path_list = ['/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/built-enviornment-conditions.csv',
-                '/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/education-income.csv',
-                '/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/employment.csv',
-                '/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/housing (1).csv',
-                '/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/land-use-zoning.csv',
-                '/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/natural-environment-conditions.csv',
-                '/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/population-density.csv',
-                '/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/public-safety.csv',
-                '/Users/shearer/Desktop/SpringResearch/data/Data/to_combine/transportation.csv']         
-    for path in path_list:
+def csv_merge(large: pd.DataFrame, paths: list):
+    for path in paths:
         df = pd.read_csv(path)
-        pitt = pitt.merge(df,on = 'Neighborhood', how = 'outer')
-        pitt.to_csv('/Users/shearer/Desktop/SpringResearch/data/test1.csv', index=False)
+        large = large.merge(df,on = 'Neighborhood', how = 'outer')
+        large.to_csv('data/test1.csv', index=False)
+
+if __name__ == "__main__":
+    pitt = pd.read_csv('data/City_of_Pittsburgh_Neighborhoods/City_of_Pittsburgh_Neighborhoods.csv')   
+    pitt = pitt.rename(columns = {'HOOD':'Neighborhood'}).set_index('Neighborhood')
+    path_list = ['data/Data/to_combine/built-environment-conditions.csv',
+                'data/Data/to_combine/education-income.csv',
+                'data/Data/to_combine/employment.csv',
+                'data/Data/to_combine/housing.csv',
+                'data/Data/to_combine/land-use-zoning.csv',
+                'data/Data/to_combine/natural-environment-conditions.csv',
+                'data/Data/to_combine/population-density.csv',
+                'data/Data/to_combine/public-safety.csv',
+                'data/Data/to_combine/transportation.csv']  
+           
+    csv_merge(path_list)
