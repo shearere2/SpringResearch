290f1747f7f629d8d4ce221b7464aeec46121424
shearere2 <shearere2@duq.edu>
2024-02-18 17:23:25 -0500

    .

--- SpringResearch/busStop.py
+++ SpringResearch/busStop.py
@@ -7,12 +7,11 @@ class busStop:
         self._routes = routes
     
 def stop(df:pd.DataFrame) -> pd.DataFrame:
-    for row in range(len(df)):
-        df['stop'] = busStop(df['Latitude'][row], df['Longitude'][row],df['Routes_ser'][row])
+    df['stop'] = str(df['Latitude']) + str(df['Longitude']) + str(df['Routes_ser'])
     return df
 
 
 if __name__ == "__main__":
     df = pd.read_csv('data/PAAC_Stops_1909.csv')
-    df = df.apply(stop)
-    print(df['stop'][1]._lat)
+    df = stop(df)
+    
