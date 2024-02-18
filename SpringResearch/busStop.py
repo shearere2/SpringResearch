c1adda682da028d4b706884b215abe17ef8385c7
shearere2 <shearere2@duq.edu>
2024-02-18 15:57:33 -0500

    one commit

--- SpringResearch/busStop.py
+++ SpringResearch/busStop.py
@@ -2,14 +2,17 @@ import pandas as pd
 class busStop:
 
     def __init__(self, lat:float, lon:float, routes:list):
-        self.lat = lat
-        self.lon = lon
-        self.routes = routes
+        self._lat = lat
+        self._lon = lon
+        self._routes = routes
+    
+def stop(df:pd.DataFrame) -> pd.DataFrame:
+    for row in range(len(df)):
+        df['stop'] = busStop(df['Latitude'][row], df['Longitude'][row],df['Routes_ser'][row])
+    return df
 
-def make_class(df:pd.DataFrame) -> pd.DataFrame:
-    df['stop'] = busStop(df['Latitude'], df['Longitude'],[0,1])
 
 if __name__ == "__main__":
     df = pd.read_csv('data/PAAC_Stops_1909.csv')
-    df = df.apply(make_class)
-    print(df['stop'])
+    df = df.apply(stop)
+    print(df['stop'][1]._lat)
