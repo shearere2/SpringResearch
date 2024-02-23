3e434100af159af4ef66e5fc6c4fdb69eb1a5029
shearere2 <shearere2@duq.edu>
2024-02-23 16:19:24 -0500

    Made busNetwork and busStop classes super clean and readable. A couple small fixes/improvements to be made.

--- SpringResearch/busNetwork.py
+++ SpringResearch/busNetwork.py
@@ -5,34 +5,65 @@ import shapely
 class busNetwork:
 
     def __init__(self):
+        """Initialize stops as the holder of all stops in the network"""
         self._stops = []
 
     # Mutators
     def add_stop(self,stop:busStop):
+        """Add a stop to the network
+
+        Args:
+            stop (busStop): stop to be added
+        """
         self._stops.append(stop)
+
     def del_stop(self,stop:busStop):
+        """Delete a stop from the network
+
+        Args:
+            stop (busStop): stop to be deleted
+        """
         self._stops.remove(stop)
 
     # Accessors
     def get_stops(self) -> list:
+        """Returns the list of the stops in the network
+
+        Returns:
+            list: all stops within the network
+        """
         return self._stops
 
 
-    def create_network():
-        network = busNetwork()
+    def create_city_network(self) -> object:
+        """Creates the bus network within the city of Pittsburgh.
+        Please note that all bus stops from outside of the city
+        limits but still a part of PRT's network have been
+        EXCLUDED.  Additionally, I also currently have to remove
+        all bus stops from multipolygon neighborhoods, like
+        Lemington-Lincoln-Belmar, unfortunately.
+
+        Returns:
+            busNetwork: The cleaned, created city network of
+            bus stops.
+        """
+        network = busNetwork() #Initialize busNetwork object
+
         df = pd.read_csv('data/PAAC_Stops_1909/PAAC_Stops_1909.csv')
-        df['stop'] = df.apply(lambda row: busStop.busStop(row['Latitude'],row['Longitude'],row['Routes_ser']), axis=1)
-        stops = df['stop']
-        for stop in stops: network.add_stop(stop)
+        df['stop'] = df.apply(lambda row: # New column in df containing busStop object, using other columns
+                              busStop.busStop(row['Latitude'],
+                                              row['Longitude'],
+                                              row['Routes_ser'],
+                                              row['Stop_name']), axis=1)
+        stops = df['stop'] # Make a series of stops from the bus stops in df
+        for stop in stops: network.add_stop(stop) # Add each stop to the network
 
-        hoods = gpd.read_file('data/pittsburgh_outline.shp')
+        hoods = gpd.read_file('data/pittsburgh_outline.shp') # Geopandas df of neighborhoods shapes
 
-
-
+        
+        """THIS SECTION BELOW IS WHAT CREATES THE COMPARABLE SHAPES AND POINTS"""
         # List of Dataframes that will later be concatenated into one large dataframe
         pre_dfs = []
-
-        # Looping over all "rows" in `my_list`
         for item in stops:
             # Generating a shapely geometry
             geometry = shapely.geometry.Point(item.get_coords())
@@ -44,48 +75,46 @@ class busNetwork:
             
             # Appending this single-row-DataFrame to the `pre_dfs` list
             pre_dfs.append(temp_df)
-
-        # Concatenating all the separate dataframes into one big DataFrame
+        
         single_df = pd.concat(pre_dfs, ignore_index=True).reset_index(drop=True)
-
-        # Finally, generating the actual GeoDataFrame that can be manipulated
         geo_df = gpd.GeoDataFrame(single_df,
                                 geometry='geometry',
                                 crs='epsg:4326')
 
-
+        """Below is the most important part; this is where a stop
+        is given a hood"""
 
         for i in range(len(hoods)):
             for j in range(len(geo_df)):
-                # if type(hoods.loc[i]['geometry'].geom_type == "MultiPolygon"):
-
-                #     print()
-
-                #     polys = list(shapely.Polygon(hoods.loc[i]['geometry']).geoms)
-                #     for q in range(len(polys)):
-
-                #         if (shapely.Polygon(polys[q]).contains(shapely.Point(geo_df.loc[j].geometry))):
-                #             network._stops[j].set_neighborhood(hoods.loc[i]['HOOD'])
-                #     print(type(hoods.loc[i]['geometry']))
                 if (hoods.loc[i]['geometry']).contains((geo_df.loc[j].geometry)):
                     network._stops[j].set_neighborhood(hoods.loc[i]['HOOD']) # THIS LINE WORKS CORRECTLY DO NOT MODIFY
 
+        
+        # \/ CURRENT WORKING SECTION: FIX THIS PART \/
+        """BELOW removes all empty string neighborhood stops.
+        (Does not work perfectly yet)"""
+        pop_tester = False
+        for i in range(len(network._stops)):
+            if pop_tester: i -= 1; pop_tester = False
+            if i >= len(network._stops):
+                break
+            elif network._stops[i].get_neighborhood() == "":
+                network._stops.pop(i)
+                pop_tester = True
         return network
     
 
 if __name__ == "__main__":
-    pitt = busNetwork.create_network()
+    pitt_no_multipolys = busNetwork.create_city_network()
     
-    stops = pitt.get_stops()
+    stops = pitt_no_multipolys.get_stops() # Stops within the network.
 
-    print()
-
-    print(stops[0].get_neighborhood()) # WHY is this giving blank line
-    # In other words, why isn't the neighborhood being set to the
-    # stop in line 62?
+    print() # Testing point
 
 
-# Will then use neighborhoods dataframe to give each row (neighborhood)
-# a list of busStops
+
+# WHAT IS NEXT ON MY PLAN:
+"""Will then use neighborhoods dataframe to give each row (neighborhood)
+a list of busStops"""
 
 
--- SpringResearch/busStop.py
+++ SpringResearch/busStop.py
@@ -1,43 +1,116 @@
 import pandas as pd
 class busStop:
 
-    def __init__(self, lat:float, lon:float, routes:list):
+    def __init__(self, lat:float, lon:float, routes:list, descr:str):
+        """Initializes a bus stop
+
+        Args:
+            lat (float): latitude
+            lon (float): longitude
+            routes (list): routes the bus stop services
+            descr (str): PRT's location description of the stop
+        """        
         self._lat = lat
         self._lon = lon
         self._routes_served = routes
-        # self._neighbors = []
+        # self._neighbors = [] # To be added later on
         self._neighborhood = ""
-        self._description = ""
+        self._description = descr
 
     """Accessor Methods"""
     def get_coords(self) -> tuple:
+        """Get the coordinates of the bus stop
+        in the form (Latitude, Longitude), which
+        is standard notation
+
+        Returns:
+            tuple: (Latitude, Longitude)
+        """
         return (self._lon,self._lat)
+    
     def get_routes(self) -> list:
+        """Returns a list of all routes that
+        the bus stop services.
+
+        Returns:
+            list: All routes the bus stop services
+        """
         return self._routes_served
+    
     def get_neighborhood(self) -> str:
+        """Returns the neighborhood that the bus stop
+        geographically falls within (according to its
+        coordinates).
+
+        Returns:
+            str: Neighborhood
+        """
         return self._neighborhood
+    
     def get_description(self) -> str:
+        """Returns the description of the bus stop
+        given to it by PRT.  This usually comes
+        in the form of an intersection, or nearby
+        monument.
+
+        Returns:
+            str: Description of the bus stop
+        """        
         return self._description
 
     """Mutator Methods"""
-    def add_neighbor(self, neighbor:str):
-        self._neighbors.append(neighbor)
-    def del_neighbor(self, neighbor:str):
-        self._neighbors.remove(neighbor)
+    # def add_neighbor(self, neighbor:str):
+    #     """Add a neighbor to the bus stop's neighbors
+
+    #     Args:
+    #         neighbor (str): Stop to become neighbor
+    #     """
+    #     self._neighbors.append(neighbor)
+
+    # def del_neighbor(self, neighbor:str):
+    #     """Delete a neighbor from the bus stop's neighbors
+
+    #     Args:
+    #         neighbor (str): Stop to remove from neighbors
+    #     """
+    #     self._neighbors.remove(neighbor)
+
     def add_route(self, route:str):
+        """Add a route for the stop to service
+
+        Args:
+            route (str): New route ID
+        """
         self._routes_served.append(route)
+
     def del_route(self, route:str):
+        """Delete a route from this stops serviced routes
+
+        Args:
+            route (str): Route ID to be removed
+        """
         self._routes_served.remove(route)
+
     def set_neighborhood(self, hood:str):
+        """Sets the neighborhood that the busstop is in.
+        All functionalities of this are done within the
+        busNetwork class's create_network function.
+
+        Args:
+            hood (str): Neighborhood the stop falls within
+        """
         self._neighborhood = hood
+
     def set_description(self, desc: str):
+        """Sets the locational description of the stop
+
+        Args:
+            desc (str): Locational description of the stop
+        """
         self._description = desc
 
 
 
 if __name__ == "__main__":
-
-    df = pd.read_csv('data/PAAC_Stops_1909.csv')
-    df['stop'] = df.apply(lambda row: busStop(row.Latitude,row.Longitude,row.Routes_ser), axis=1)
-    #df.to_csv('data/stop_test.csv')
-    print(df['stop'][0].get_coords()[0]) # Latitude of first stop in df
+    
+    print()
