d2ec2185a274b0a0fa58041fe7dabbbae9eb55d5
shearere2 <shearere2@duq.edu>
2024-03-13 19:22:34 -0400

    Created elevation difference function

--- SpringResearch/walksheds_folder/find_elevation.py
+++ SpringResearch/walksheds_folder/find_elevation.py
@@ -8,9 +8,19 @@ def find_elevation(lat:float, long:float) -> float:
     file = json.loads(string)
     '''I can be so specific on this return becuase of how
     specific I know the json format will be.'''
-    return file['results'][0]['elevation'] 
+    return file['results'][0]['elevation']
+
+
+def elevation_difference(start:tuple,end:tuple) -> float:
+    start_elev = find_elevation(start[0],start[1])
+    end_elev = find_elevation(end[0],end[1])
+
+    return end_elev - start_elev # Represents meters that must be walked
+#                                  uphill to get from point 1 to point 2
+
+
+
 
 if __name__ == "__main__":
-    lat = float(input('Enter the latitude: '))
-    long = float(input('Enter the longitude: '))
-    print(f'Elevation at ({lat},{long}): {str(find_elevation(lat,long))} meters above sea level.')
+    change = elevation_difference((40,-79.8),(40.01,-79.9))
+    print(f'To get from point one to point two, you will have to go up {change} meters')
