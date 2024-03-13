fed4f72d69844d20b723f15dc43d3bf23ded2442
shearere2 <shearere2@duq.edu>
2024-03-13 17:16:28 -0400

    Function to find elevation complete!

--- SpringResearch/walksheds_folder/find_elevation.py
+++ SpringResearch/walksheds_folder/find_elevation.py
@@ -6,12 +6,11 @@ def find_elevation(lat:float, long:float) -> float:
     response = requests.get(url)
     string = response.content.decode()
     file = json.loads(string)
-    print()
     '''I can be so specific on this return becuase of how
     specific I know the json format will be.'''
-    return file['results'][0]['elevation'] # 
+    return file['results'][0]['elevation'] 
 
 if __name__ == "__main__":
     lat = float(input('Enter the latitude: '))
     long = float(input('Enter the longitude: '))
-    print(f'Elevation at ({lat},{long}): {str(find_elevation(0,0))} meters above sea level.')
+    print(f'Elevation at ({lat},{long}): {str(find_elevation(lat,long))} meters above sea level.')
