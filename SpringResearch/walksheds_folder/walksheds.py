2505d42bada332cfdd3849f90095b36c3e9b84c7
shearere2 <shearere2@duq.edu>
2024-03-12 23:51:24 -0400

    Worked on using opentopo API to be able to extract elevations given coordinates!

---
+++ SpringResearch/walksheds_folder/walksheds.py
@@ -0,0 +1,15 @@
+print()
+
+# git clone https://github.com/ajnisbet/opentopodata.git
+# cd opentopodata
+# make build
+# make run
+
+# mkdir ./data/ned10m
+
+# Ran strm format file
+
+# make build && make run
+# STUCK ON THIS ABOVE STEP, MIGHT HAVE HAD ERRORS IN FIRST STEP THAT CAUSE THIS
+
+# Ran the above to build my opentopo
