0b1c8537aa033dcc3f25b14310dd38b30d9763f4
shearere2 <shearere2@duq.edu>
2024-03-13 14:41:53 -0400

    Wrote elevation_finder function -- command line issues to be resolved.

---
+++ SpringResearch/walksheds_folder/elevation_finder.py
@@ -0,0 +1,27 @@
+import subprocess
+import json
+
+def elevation_finder(lat:float, long:float) -> float:
+
+    file=json.load(subprocess.run([f'curl https://api.opentopodata.org/v1/test-dataset?locations={lat},{long}'], shell=True))
+    for key, value in file['results'][0]: # json files are just dictionaries, so this will give us the  
+        if key == 'elevation':
+            return value
+        
+
+
+
+
+# git clone https://github.com/ajnisbet/opentopodata.git
+# cd opentopodata
+# make build
+# make run
+
+# mkdir ./data/ned10m
+
+# Ran strm format file
+
+# make build && make run
+# STUCK ON THIS ABOVE STEP, MIGHT HAVE HAD ERRORS IN FIRST STEP THAT CAUSE THIS
+
+# Ran the above to build my opentopo
