da8277ea218e4a73a9821276e99bdc8932cd69b9
shearere2 <shearere2@duq.edu>
2024-03-15 14:02:51 -0400

    Now opens a webpage with hello django, using a base template.

--- elevation/views.py
+++ elevation/views.py
@@ -1,4 +1,14 @@
+from django.shortcuts import render
+from datetime import datetime
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Hello, Django!")
+def hello_there(request, name):
+    print(request.build_absolute_uri()) #optional
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
