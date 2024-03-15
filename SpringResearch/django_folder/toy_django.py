27155e507f0569c4a125d5709c22eb1780da641b
shearere2 <shearere2@duq.edu>
2024-03-15 12:49:03 -0400

    Created django toy web app. To be converted to useful app

--- SpringResearch/django_folder/toy_django.py
+++ SpringResearch/django_folder/toy_django.py
@@ -2,16 +2,17 @@ from django.shortcuts import render
 import geocoder
 import requests
 from datetime import datetime
+from django.http import HttpResponse
 
-def temp_here():
+def temp_here(request):
     endpoint = "https://api.open-meteo.com/v1/forecast"
     location = geocoder.ip('me').latlng
-    api_request = f"{endpoint}?latitude={location[0]}&longitude={location[1]}&hourly=temperature_2m"
+    api_request = f"{endpoint}?latitude={location[0]}&longitude={location[1]}&hourly=temperature_2m&temperature_unit=fahrenheit"
     now = datetime.now()
     hour = now.hour
     meteo_data = requests.get(api_request).json()
     temp = meteo_data['hourly']['temperature_2m'][hour]
-    return temp
+    return HttpResponse(f"Here, it's {temp}")
 
 if __name__ == "__main__":
     print(temp_here())
